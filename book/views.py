205a23a522cfa24b7cfa773d84afecd79d5124da
tim0114gap <tim.0114.gap@gmail.com>
2019-05-16 19:37:10 +0900

    fixed

--- book/views.py
+++ book/views.py
@@ -9,6 +9,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Resolving URLs
 from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
 
 # Import Book Model
 from book.models import Book
@@ -28,9 +29,22 @@ class BookView(DetailView):
 class BookCreate(CreateView):
     model = Book
     form_class = BookForm
+    template_name = 'book/book_form.html'
     # Setting returning URL
     success_url = reverse_lazy('book_list')
 
+    def post(self, request, *args, **kwargs):
+        forms = BookForm()
+        if request.method == 'POST':
+            forms = BookForm(request.POST)
+
+            if forms.is_valid():
+                new_message = Book(titel=request.POST['title'],text=request.POST['text'],image=request.POST['image'])
+                new_message.save()
+
+                success_url = reverse_lazy('book_list')
+
+        return redirect('book_list')
 # Update View
 class BookUpdate(UpdateView):
     model = Book
